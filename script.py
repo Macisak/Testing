57449b09d5e80e2b6c317110364b02402b643804
Unknown <martin@socifi.com>
2018-03-22 14:08:39 +0100

    Big issue 2

--- script.py
+++ script.py
@@ -2,7 +2,28 @@
 
 # Pepa píše program
 
-# Pepa je chytrý, proto vyvíjí v dev
+# Pepa je inteligentní, proto vyvíjí v dev
+
+# Pepa si mezitím, co Franta vyvíjí novou featuru, všiml, že program je špatně napsaný a potenciálně se v něm nachází případ, který by mohl vést na nezamýšlené chování.
+# Přepsal část kódu tak, aby k tomuto případu nemohlo dojít.
 
 # Na tento velký projekt přibyl Franta, který dostal za úkol implementovat novou featuru (Dělá to, že nedělá nic)
 # (Tyto dva řádky napsal Franta)
+
+# Franta dostal specifikace od Jany a Amálky a mohl tak začít programovat na své části projektu
+# Jeho úkolem bylo naprogramovat
+#  - funkci, která volá Amálčinu funkci, pak dělá nějaké výpočty a nakonec nevrátí nic
+#  - funkci, která volá v cyklu Amálčinu funkci
+
+from importantFunction import importantFunction
+
+def functionThatDoesNothing1():
+	importantFunction()
+	a = 1
+	b = 2
+	c = a + b
+	return
+
+def functionThatDoesNothing1():
+	for i in range(10):
+		importantFunction()
