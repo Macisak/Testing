35cf1ce69ab01fe4573cfe2fa62c2b3d54a396be
Unknown <martin@socifi.com>
2018-03-22 14:07:20 +0100

    Big feature

--- script.py
+++ script.py
@@ -2,7 +2,13 @@
 
 # Pepa píše program
 
-# Pepa je chytrý, proto vyvíjí v dev
+# Pepa je inteligentní, proto vyvíjí v dev
+
+# Pepa si mezitím, co Franta vyvíjí novou featuru, všiml, že program je špatně napsaný a potenciálně se v něm nachází případ, který by mohl vést na nezamýšlené chování.
+# Přepsal část kódu tak, aby k tomuto případu nemohlo dojít.
 
 # Na tento velký projekt přibyl Franta, který dostal za úkol implementovat novou featuru (Dělá to, že nedělá nic)
 # (Tyto dva řádky napsal Franta)
+
+
+# Na projekt přibyla projektová manažerka Jana a nová juniorní vývojářka Amálka. Klient požaduje vytvoření nové velké featury. Jana proto vzala specifikace a protože se daná featura skládá z několika dílčích úkolů, rozdělila ji na tyto úkoly. První a nejlehčí část dostala na starost Amálka. Pro ní byla založena větev issue/big-feature-1 a má za úkol naprogramovat funkci, která nedělá nic.
